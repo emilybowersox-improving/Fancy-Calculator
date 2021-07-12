acff91e7075bce47cf6e1b23e941f548e8b67828
Emily Bowersox <emily.bowersox@improving.com>
2021-07-12 08:46:29 -0500

    #2 - Checking Inputs

--- FancyCalculator/Program.cs
+++ FancyCalculator/Program.cs
@@ -6,22 +6,42 @@ namespace FancyCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number");
-
+            Console.WriteLine("Fancy Calculator");
+     
             string userInput1 = "";
             string userInput2 = "";
             decimal number1 = 0;
             decimal number2 = 0;
             decimal results = 0;
 
-            userInput1 = Console.ReadLine();
+            do
+            { 
+                Console.WriteLine("Enter a number");
+                userInput1 = Console.ReadLine();
+                if (decimal.TryParse(userInput1, out number1) == true)
+                {
+                    break;
+                } else
+                {
+                    Console.WriteLine(userInput1 + " is not a valid number");
+                }
+            }  while (decimal.TryParse(userInput1, out number1) == false);
 
-            Console.WriteLine("Enter a second number");
 
-            userInput2 = Console.ReadLine();
+            do
+            {
+                Console.WriteLine("Enter another number");
+                userInput2 = Console.ReadLine();
+                if (decimal.TryParse(userInput2, out number2) == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(userInput2 + " is not a valid number");
+                }
+            }   while (decimal.TryParse(userInput2, out number2) == false);
 
-            number1 = Convert.ToDecimal(userInput1);
-            number2 = Convert.ToDecimal(userInput2);
 
             results = number1 + number2;
 
