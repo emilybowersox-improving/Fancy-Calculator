2a7dc989a8cc742ebc6d22d898bad6fc1c34ad92
Emily Bowersox <emily.bowersox@improving.com>
2021-07-12 09:12:07 -0500

    #3 - Take in math equation (addition)

--- FancyCalculator/Program.cs
+++ FancyCalculator/Program.cs
@@ -7,46 +7,39 @@ namespace FancyCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Fancy Calculator");
-     
-            string userInput1 = "";
-            string userInput2 = "";
+
             decimal number1 = 0;
             decimal number2 = 0;
             decimal results = 0;
 
-            do
-            { 
-                Console.WriteLine("Enter a number");
-                userInput1 = Console.ReadLine();
-                if (decimal.TryParse(userInput1, out number1) == true)
-                {
-                    break;
-                } else
-                {
-                    Console.WriteLine(userInput1 + " is not a valid number");
-                }
-            }  while (decimal.TryParse(userInput1, out number1) == false);
-
+            string fullInput = "";
+            char[] delimiterChars = { ' ', '+' };
+            string[] inputs = new string[2];
+                
+            fullInput.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
 
             do
-            {
-                Console.WriteLine("Enter another number");
-                userInput2 = Console.ReadLine();
-                if (decimal.TryParse(userInput2, out number2) == true)
                 {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine(userInput2 + " is not a valid number");
-                }
-            }   while (decimal.TryParse(userInput2, out number2) == false);
+                Console.WriteLine("Enter what you would like to see added:");
+                fullInput = Console.ReadLine();
+                inputs = fullInput.Split(delimiterChars, StringSplitOptions.RemoveEmptyEntries);
+                if (decimal.TryParse(inputs[0], out number1) == true && decimal.TryParse(inputs[1], out number2) == true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(fullInput + " is not a valid expression");
+                    }
+                } while (decimal.TryParse(inputs[0], out number1) == false || decimal.TryParse(inputs[1], out number2) == false);
 
-
+  
             results = number1 + number2;
 
             Console.WriteLine("Your result is " + results);
             Console.ReadLine();
+
+
 
 
         }
