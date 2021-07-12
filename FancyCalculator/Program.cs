132971a730a8e238cc2f777f78625f57b5536581
Emily Bowersox <emily.bowersox@improving.com>
2021-07-12 08:22:48 -0500

    #1 - Basic Addition

--- FancyCalculator/Program.cs
+++ FancyCalculator/Program.cs
@@ -6,7 +6,29 @@ namespace FancyCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number");
+
+            string userInput1 = "";
+            string userInput2 = "";
+            decimal number1 = 0;
+            decimal number2 = 0;
+            decimal results = 0;
+
+            userInput1 = Console.ReadLine();
+
+            Console.WriteLine("Enter a second number");
+
+            userInput2 = Console.ReadLine();
+
+            number1 = Convert.ToDecimal(userInput1);
+            number2 = Convert.ToDecimal(userInput2);
+
+            results = number1 + number2;
+
+            Console.WriteLine("Your result is " + results);
+            Console.ReadLine();
+
+
         }
     }
 }
