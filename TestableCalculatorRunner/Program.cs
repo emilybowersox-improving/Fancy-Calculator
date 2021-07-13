4eebf52a7737c0e4b603032773d7a5880831d2ad
Emily Bowersox <emily.bowersox@improving.com>
2021-07-13 10:40:43 -0500

    TestableCalculatorRunner exits at exit

--- TestableCalculatorRunner/Program.cs
+++ TestableCalculatorRunner/Program.cs
@@ -7,24 +7,32 @@ namespace TestableCalculatorRunner
     {
         static void Main(string[] args)
         {
+            string userInput = "";
             // console stuff goes here
             var calculator = new Calculator();
 
             Console.WriteLine("Fancy Calculator");
 
-            Console.WriteLine("Enter what you would like to see added:");
-            string userInput = Console.ReadLine();
-
-            var myResults = calculator.Evaluate(userInput);
-
-
-            if (myResults.Result != 0)
+            do
             {
-                Console.WriteLine(myResults.Result);
-            } else
-            {
-                Console.WriteLine(myResults.ErrorMessage);
-            }
+                Console.WriteLine("Enter what you would like to see added:");
+                userInput = Console.ReadLine();
+
+                var myResults = calculator.Evaluate(userInput);
+
+                if (myResults.Result != 0)
+                {
+                    Console.WriteLine(myResults.Result);
+                }
+                else
+                {
+                    Console.WriteLine(myResults.ErrorMessage);
+                }
+            } while (userInput != "exit");
+
+        
+
+
         }
     }
 }
